cf6abb346fc6a858dc43ad3d26a9b1272777692d
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-01-03 20:19:54 +0100

    Add navigation links

--- index.js
+++ index.js
@@ -3,4 +3,22 @@ $(function() {
   $(window).on('load resize', function() {
     $('.fill-screen').css('height, window.innerHeight');
   });
+
+  // scrollspy
+  $(body).scrollspy({ target: '.nav' });
 });
+
+// Funtion to initalise scrollspy funtionality
+// $(function() {
+//   var $nav = $('#nav');
+
+//   $('#sticky-navigation').scrollspy({
+//     min: $nav.offset().top,
+//     onEnter: function(element, position) {
+//       $nav.addClass('fixed');
+//     },
+//     onLeave: function(element, position) {
+//       $nav.removeClass('fixed');
+//     },
+//   });
+// });
